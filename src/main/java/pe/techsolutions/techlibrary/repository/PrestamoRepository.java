d16d41efcc843f2faee39fc9a8c99d4118f069d0
renzo-fn <renzoyupanqui29@gmail.com>
2026-04-30 09:38:04 -0500

    refactor(PrestamoRepository): Editar lineas de codigo del repositorio - Se cambio algunas lineas de codigo del repositorio ya que estaban mal

--- src/main/java/pe/techsolutions/techlibrary/repository/PrestamoRepository.java
+++ src/main/java/pe/techsolutions/techlibrary/repository/PrestamoRepository.java
@@ -3,7 +3,7 @@ package pe.techsolutions.techlibrary.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.techsolutions.techlibrary.model.Prestamo;
 
-java.util.List;
+import java.util.List;
 
 public interface PrestamoRepository extends JpaRepository <Prestamo, Long> {
     List<Prestamo> findByUsuarioId(Long usuarioId);
